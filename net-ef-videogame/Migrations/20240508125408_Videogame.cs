95e36b478e0eb3452aed7042cf4980a2e652b33a
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-08 15:11:51 +0200

    problem migrations

---
+++ net-ef-videogame/Migrations/20240508125408_Videogame.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net_ef_videogame.Migrations
+{
+    /// <inheritdoc />
+    public partial class Videogame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "videogame",
+                columns: table => new
+                {
+                    VideogameID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    videogame_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    videogame_description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    videogame_release = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_videogame", x => x.VideogameID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "software_houses",
+                columns: table => new
+                {
+                    HouseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    videogame_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    videogame_tax_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    videogame_city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    videogame_country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VideogameID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_software_houses", x => x.HouseID);
+                    table.ForeignKey(
+                        name: "FK_software_houses_videogame_VideogameID",
+                        column: x => x.VideogameID,
+                        principalTable: "videogame",
+                        principalColumn: "VideogameID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_software_houses_videogame_tax_id",
+                table: "software_houses",
+                column: "videogame_tax_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_software_houses_VideogameID",
+                table: "software_houses",
+                column: "VideogameID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "software_houses");
+
+            migrationBuilder.DropTable(
+                name: "videogame");
+        }
+    }
+}
---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+
+    [Table("videogame")]
+    
+    public class Videogame
+    {
+        [Key] public int VideogameID { get; set; }
+
+        [Column("videogame_name")]
+        public string name { get; set; }
+
+        [Column("videogame_description")]
+        public string description { get; set; }
+
+        [Column("videogame_release")]
+        public DateTime release {  get; set; }
+
+        public DateTime created_at { get; set; } = DateTime.Now;
+        public DateTime updated_at { get; set; } = DateTime.Now;
+
+
+        //vincolo N a 1
+        public List<Software_House> houses { get; set; }
+
+        /*public Videogame(int Id, string Name, string Description, DateTime Release, DateTime Created_at, DateTime Updated_at)
+        {
+            VideogameID = Id;
+            name = Name;
+            description = Description;
+            release = Release;
+        }*/
+    }
+
+    [Index(nameof(tax_id), IsUnique = true)]
+    public class Software_House
+    {
+        [Key] public int HouseID { get; set; }
+
+        [Column("software_house_name")]
+        public string name { get; set; }
+
+        [Column("software_house_tax_id")]
+        public string tax_id { get; set; }
+
+        [Column("software_house_city")]
+        public string city { get; set; }
+
+        [Column("software_house_country")]
+        public string country { get; set; }
+        public DateTime created_at { get; set; } = DateTime.Now;
+        public DateTime updated_at { get; set; } = DateTime.Now;
+
+
+        //Vincolo 1 a N
+        public int VideogameID { get; set; }
+        public Videogame videogame { get; set; }
+    }
+}
